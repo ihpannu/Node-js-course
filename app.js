ae1a607ce270c19fd577f76f87079a3f96e06d4a
Harman Pannu <iharmanpannu@gmail.com>
2018-03-27 01:58:51 -0700

    changes

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ const EventEmitter = require('events');
 
 const myEmitter = new EventEmitter();
 
+// Register a listner
+myEmitter.on('messageLogged', () => {
+  console.log('listner called');
+});
+
+// Raise an event
 myEmitter.emit('messageLogged');
