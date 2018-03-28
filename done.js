dd9a5bc3352539289be57a54fa9e544f2087d539
Harman Pannu <iharmanpannu@gmail.com>
2018-03-28 12:37:12 -0700

    finished with http module

--- done.js
+++ done.js
@@ -26,3 +26,27 @@ logger.on('messageLogged', arg => {
   console.log('Listner Called', arg);
 });
 logger.log('message');
+
+// how to create a server
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('Hello World');
+    res.end();
+  }
+  if (req.url === '/api/courses') {
+    res.write(JSON.stringify([1, 2, 3, 4, 5]));
+    res.end();
+  }
+  if (req.url === '/about') {
+    res.write('This is a about page');
+    res.end();
+  }
+});
+
+server.on('connection', socket => {
+  console.log('New Connection...');
+});
+server.listen(3000);
+console.log('listening on port 3000...');
