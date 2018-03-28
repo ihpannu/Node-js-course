2df55f5ec0f46f113863c7d723c754ed2264c017
Harman Pannu <iharmanpannu@gmail.com>
2018-03-28 01:26:31 -0700

    changes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const EventEmitter = require('events');
-const emitter = new EventEmitter();
 
-emitter.on('messageLogged', arg => {
+const Logger = require('./logger');
+const logger = new Logger();
+logger.log('message');
+
+logger.on('messageLogged', arg => {
   console.log('Listner Called', arg);
 });
-
-const log = require('./logger');
-log('message');
