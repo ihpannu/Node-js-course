db597756c18b92f6d14aea4277809b1187d32d6e
Harman Pannu <iharmanpannu@gmail.com>
2018-03-28 01:17:14 -0700

    changes events

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 const EventEmitter = require('events');
-
 const emitter = new EventEmitter();
 
 emitter.on('messageLogged', arg => {
   console.log('Listner Called', arg);
 });
-// Raise an event
-emitter.emit('messageLogged', {
-  id: 1,
-  url: 'http://'
-});
 
-emitter.on('logging', data => {
-  console.log('data', data);
-});
-emitter.emit('logging', {
-  data: 5,
-  name: 'Harman'
-});
+const log = require('./logger');
+log('message');
--- logger.js
+++ logger.js
@@ -1,11 +1,17 @@
-console.log(__filename);
-console.log(__dirname);
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
 var url = 'http://example.com/log';
 
 function log(message) {
   //send the HTTP request¯
   console.log('Hi ' + message);
+
+  // Raise an event
+  emitter.emit('messageLogged', {
+    id: 1,
+    url: 'http://'
+  });
 }
 log('Harman');
 module.exports = log;
