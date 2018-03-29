fdb9524fc0b994638ba05bc2b72946caae80a714
Harman Pannu <iharmanpannu@gmail.com>
2018-03-29 14:55:48 -0700

    learned put

--- demos/express-demo/index.js
+++ demos/express-demo/index.js
@@ -17,13 +17,12 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  };
-  const result = Joi.validate(req.body, schema);
-  console.log(result);
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -40,5 +39,30 @@ app.get('/api/courses/:id', (req, res) => {
   res.send(course);
 });
 
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(400).send('This is invalid ID');
+  }
+
+  const { error } = validateCourse(req.body); // object destructure
+
+  if (error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  course.name = req.body.name;
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string()
+      .alphanum()
+      .min(3)
+      .required()
+  };
+  return Joi.validate(course, schema);
+}
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
