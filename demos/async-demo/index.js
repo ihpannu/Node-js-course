04dfc67e03504a639d91d0cd06bb9e5301bb8513
Harman Pannu <iharmanpannu@gmail.com>
2018-03-30 12:10:54 -0700

    asynch javascript

--- demos/async-demo/index.js
+++ demos/async-demo/index.js
@@ -1,15 +1,21 @@
 console.log('Before');
-const user = getUser(1);
-console.log(user);
+getUser(1, (user) => {
+  console.log('User: ', user);
+});
+
 console.log('After');
 
-function getUser(id) {
+function getUser(id, callback) {
   setTimeout(() => {
     console.log('Reading a user from database...');
-    return {
+    callback({
       id: id,
       githubUsername: 'Harman'
-    };
+    });
   }, 2000);
-  return 1;
+
+}
+
+function getRepositories(username) {
+  return ['repo1', 'repo2', 'repo3']
 }
