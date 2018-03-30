84426344c8a6b4aec519b1e2756cc00c2c3cc9b6
Harman Pannu <iharmanpannu@gmail.com>
2018-03-30 13:34:05 -0700

    Promises

---
+++ demos/async-demo/callbacks.js
@@ -0,0 +1,29 @@
+console.log('Before');
+getUser(1, getRepositories);
+console.log('After');
+
+function getRepositories(user) {
+  getRepositories(user.githubUsername, getCommits);
+}
+function getCommits(repos) {
+  getCommits(repo, displayCommits);
+}
+function displayCommits(commits) {
+  console.log(commits);
+}
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log('Reading a user from database...');
+    callback({
+      id: id,
+      githubUsername: 'Harman'
+    });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log('Calling GitHub API...');
+    callback(['repo1', 'repo2', 'repo3']);
+  }, 2000);
+}
--- demos/async-demo/index.js
+++ demos/async-demo/index.js
@@ -1,30 +1,3 @@
 console.log('Before');
-getUser(1, user => {
-  getRepositories(user.githubUsername, repo => {
-    getCommits();
-  });
-});
 
 console.log('After');
-function getCommits(repo, displayCommits) {
-  console.log(repo, displayCommits);
-}
-function displayCommits(commits) {
-  console.log(commits);
-}
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log('Reading a user from database...');
-    callback({
-      id: id,
-      githubUsername: 'Harman'
-    });
-  }, 2000);
-}
-
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log('Calling GitHub API...');
-    callback(['repo1', 'repo2', 'repo3']);
-  }, 2000);
-}
