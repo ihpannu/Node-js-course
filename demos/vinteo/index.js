0c3465523bd420d893c9b906f29451304decccd1
Harman Pannu <iharmanpannu@gmail.com>
2018-03-29 17:51:19 -0700

    added second method

--- demos/vinteo/index.js
+++ demos/vinteo/index.js
@@ -21,12 +21,44 @@ app.get('/api/genres', (req, res) => {
 });
 
 // GET METHOD FOR SINGLE COURSE
-
 app.get('/api/genres/:id', (req, res) => {
   const movies = genres.find(genre => genre.id === parseInt(req.params.id));
   if (!movies) return res.status(400).send('This is invalid ID ');
   return res.send(movies);
 });
+
+// POST METHOD
+app.post('/api/genres', (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  };
+  genres.push(genre);
+  res.send(genre);
+});
+
+app.put('/api/genres/:id', (req, res) => {
+  const { error } = validateGenre(res.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = {
+    name: req.body.name
+  };
+  res.send(genre);
+});
+
+// FUNCTION TO VALIDATE ENTRIES
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+  return Joi.validate(genre, schema);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Listening at 3000'));
