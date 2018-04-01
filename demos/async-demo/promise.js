2ee8d2660e1afb7ca12510df7e3ce9ff3fbaddc9
Harman Pannu <iharmanpannu@gmail.com>
2018-04-01 14:41:13 -0700

    promises object

--- demos/async-demo/promise.js
+++ demos/async-demo/promise.js
@@ -1,13 +1,18 @@
-const p = new Promise((resolve, reject) => {
-  // Kick off some async work
-  //....
-  setTimeout(() => {
-    // resolve(1);
-    reject(new Error('message'));
-  }, 2000);
-});
+// const p = new Promise((resolve, reject) => {
+//   // Kick off some async work
+//   //....
+//   setTimeout(() => {
+//     // resolve(1);
+//     reject(new Error('message'));
+//   }, 2000);
+// });
 
-p
-  .then(result => {
-    console.log('Result:', result);
-  .catch(err => console.log('Error:', err.message));
+// p
+//   .then(result => {
+//     console.log('Result:', result);
+//   })
+//   .catch(err => console.log('Error:', err.message));
+
+const p = new Promise((resolve, reject) => {
+  //Kick off some async work
+});
