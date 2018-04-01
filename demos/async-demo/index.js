06c641bed0f7835add3fdeec38d67db076a42ac4
Harman Pannu <iharmanpannu@gmail.com>
2018-04-01 15:16:14 -0700

    promise api

--- demos/async-demo/index.js
+++ demos/async-demo/index.js
@@ -1,3 +1,44 @@
 console.log('Before');
 
+// getUser(1, user => {
+//   getRepositories(user.githubUsername, repos => {
+//     getCommits(repo[0], commits => {
+//       console.log(commits);
+//     });
+//   });
+// });
+
+getUser(1)
+  .then(user => getRepositories(user.githubUsername))
+  .then(repo => getCommits(repos[0]))
+  .then(commits => console.log('Commits', commits))
+  .catch(err => console.log('Error', err.message));
+
 console.log('After');
+
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Reading a user from database...');
+      resolve({
+        id: id,
+        githubUsername: 'Harman'
+      });
+    }, 2000);
+  });
+}
+
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+      resolve(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+  });
+}
+
+function getCommits(repos) {
+  return new Promise((resolve, reject) => {
+    getCommits(repos, displayCommits);
+  });
+}
