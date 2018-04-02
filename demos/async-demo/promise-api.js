1ec54d64f8cb5ae3070bdf4a9c916047ca5e6b71
Harman Pannu <iharmanpannu@gmail.com>
2018-04-02 11:14:13 -0700

    promise all

--- demos/async-demo/promise-api.js
+++ demos/async-demo/promise-api.js
@@ -4,3 +4,18 @@
 // p.then(result => console.log(result));
 const p = Promise.reject(new Error('Reason for rejection'));
 p.catch(err => console.log(err.message));
+
+const p1 = new Promise(resolve => {
+  setTimeout(() => {
+    console.log('Async Operation 1....');
+    resolve(1);
+  }, 2000);
+});
+const p2 = new Promise(resolve => {
+  setTimeout(() => {
+    console.log('Async Operation 2....');
+    resolve(2);
+  }, 2000);
+});
+
+Promise.all([p1, p2]).then(result => console.log(result));
