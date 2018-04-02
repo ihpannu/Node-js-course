7fd24de5c89c1c78821d471c4c5c3f1e8e6b0cbe
Harman Pannu <iharmanpannu@gmail.com>
2018-04-02 11:29:08 -0700

    finished with promises

--- demos/async-demo/promise-api.js
+++ demos/async-demo/promise-api.js
@@ -5,18 +5,13 @@
 const p = Promise.reject(new Error('Reason for rejection'));
 p.catch(err => console.log(err.message));
 
-// const p1 = new Promise(resolve => {
-//   setTimeout(() => {
-//     console.log('Async Operation 1....');
-//     resolve(1);
-//   }, 2000);
-// });
-const p1 = new Promise((resolve, reject) => {
+const p1 = new Promise(resolve => {
   setTimeout(() => {
     console.log('Async Operation 1....');
-    reject(new Error('Because something failed'));
+    resolve(1);
   }, 2000);
 });
+
 const p2 = new Promise(resolve => {
   setTimeout(() => {
     console.log('Async Operation 2....');
@@ -24,6 +19,18 @@ const p2 = new Promise(resolve => {
   }, 2000);
 });
 
-Promise.all([p1, p2])
+Promise.all([p1, p2]) // logs all
   .then(result => console.log(result))
   .catch(err => console.log('Error: ', err.message));
+
+Promise.race([p1, p2]) // log the first resolved api
+  .then(result => console.log(result))
+  .catch(err => console.log('Error: ', err.message));
+
+// Error
+// const p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log('Async Operation 1....');
+//     reject(new Error('Because something failed'));
+//   }, 2000);
+// });
