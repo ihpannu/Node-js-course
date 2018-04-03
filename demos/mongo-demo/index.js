b0858eef426dc09508c4d99ce8b2b458d89afc0f
Harman Pannu <iharmanpannu@gmail.com>
2018-04-03 01:09:43 -0700

    mongoose

--- demos/mongo-demo/index.js
+++ demos/mongo-demo/index.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground');
+mongoose
+  .connect('mongodb://localhost/playground')
+  .then(() => {
+    console.log('Connected to MongoDB....');
+  })
+  .catch(err => console.error('Could not connect to MongoDB...', err));
