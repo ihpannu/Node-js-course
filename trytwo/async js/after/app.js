d2d4182d53403e65efe4fc7308dff598cfa00887
Harman Pannu <iharmanpannu@gmail.com>
2018-04-15 17:23:01 -0700

    async js

---
+++ trytwo/async js/after/app.js
@@ -0,0 +1,21 @@
+console.log("Before");
+
+getUser(1, user => {
+  console.log("User ", user);
+});
+
+console.log("After");
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Reading a user from database....");
+    callback({
+      id: id,
+      githubUserName: "Harman",
+      repos: {
+        id: 1,
+        name: "Vue js"
+      }
+    });
+  }, 2000);
+}
