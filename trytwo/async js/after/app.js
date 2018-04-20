4101a53c78858b5beb598dd2b52c3d9568184674
Harman Pannu <iharmanpannu@gmail.com>
2018-04-19 23:41:52 -0700

    converted callbacks to promises

--- trytwo/async js/after/app.js
+++ trytwo/async js/after/app.js
@@ -1,36 +1,36 @@
-// console.log("Before");
+console.log("Before");
 
-// getUser(1, user => {
-//   console.log("User ", user);
-// });
-
-// console.log("After");
-
-// function getUser(id, callback) {
-//   setTimeout(() => {
-//     console.log("Reading a user from database....");
-//     callback({
-//       id: id,
-//       githubUserName: "Harman",
-//       repos: {
-//         id: 1,
-//         name: "Vue js"
-//       }
-//     });
-//   }, 2000);
-// }
-
-// function hotUpdate(ok) {
-//   console.log(ok);
-// }
-
-const p = new Promise(function(resolve, reject) {
-  // Kick off some async work
-  setTimeout(() => {
-    // resolve(1);
-    reject(new Error("message"));
-  }, 2000);
+getUser(1, user => {
+  console.log("User ", user);
 });
-p
-  .then(result => console.log("Result", result))
-  .catch(error => console.error("Error", error.message));
+
+console.log("After");
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Reading a user from database....");
+    callback({
+      id: id,
+      githubUserName: "Harman",
+      repos: {
+        id: 1,
+        name: "Vue js"
+      }
+    });
+  }, 2000);
+}
+
+function hotUpdate(ok) {
+  console.log(ok);
+}
+
+// const p = new Promise(function(resolve, reject) {
+//   // Kick off some async work
+//   setTimeout(() => {
+//     // resolve(1);
+//     reject(new Error("message"));
+//   }, 2000);
+// });
+// p
+//   .then(result => console.log("Result", result))
+//   .catch(error => console.error("Error", error.message));
--- trytwo/async js/after/exercise.js
+++ trytwo/async js/after/exercise.js
@@ -1,35 +1,40 @@
-
-getCustomer(1, (customer) => {
-  console.log('Customer: ', customer);
+getCustomer(1, customer => {
+  console.log("Customer: ", customer);
   if (customer.isGold) {
-    getTopMovies((movies) => {
-      console.log('Top movies: ', movies);
+    getTopMovies(movies => {
+      console.log("Top movies: ", movies);
       sendEmail(customer.email, movies, () => {
-        console.log('Email sent...')
+        console.log("Email sent...");
       });
     });
   }
 });
 
-function getCustomer(id, callback) {
-  setTimeout(() => {
-    callback({ 
-      id: 1, 
-      name: 'Mosh Hamedani', 
-      isGold: true, 
-      email: 'email' 
-    });
-  }, 4000);  
+function getCustomer(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        id: 1,
+        name: "Mosh Hamedani",
+        isGold: true,
+        email: "email"
+      }).reject(error, console.error("Error", error.message));
+    }, 4000);
+  });
 }
 
-function getTopMovies(callback) {
-  setTimeout(() => {
-    callback(['movie1', 'movie2']);
-  }, 4000);
+function getTopMovies() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(["movie1", "movie2"]);
+    }, 4000).reject(error => console.error("Error", error.message));
+  });
 }
 
-function sendEmail(email, movies, callback) {
-  setTimeout(() => {
-    callback();
-  }, 4000);
+function sendEmail(email, movies) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve().reject(err => console.error("Error", err.message));
+    }, 4000);
+  });
 }
