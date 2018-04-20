96e418078d6882adc44149ab7c920e8125dbad63
Harman Pannu <iharmanpannu@gmail.com>
2018-04-20 00:14:12 -0700

    finished with promises

--- trytwo/async js/after/exercise.js
+++ trytwo/async js/after/exercise.js
@@ -1,14 +1,20 @@
-getCustomer(1, customer => {
-  console.log("Customer: ", customer);
-  if (customer.isGold) {
-    getTopMovies(movies => {
-      console.log("Top movies: ", movies);
-      sendEmail(customer.email, movies, () => {
-        console.log("Email sent...");
-      });
-    });
-  }
-});
+// getCustomer(1, customer => {
+//   console.log("Customer: ", customer);
+//   if (customer.isGold) {
+//     getTopMovies(movies => {
+//       console.log("Top movies: ", movies);
+//       sendEmail(customer.email, movies, () => {
+//         console.log("Email sent...");
+//       });
+//     });
+//   }
+// });
+
+getCustomer(1)
+  .then(customer => console.log(customer))
+  .then(customer => if (customer.isGold) {
+
+  });
 
 function getCustomer(id) {
   return new Promise((resolve, reject) => {
@@ -18,7 +24,7 @@ function getCustomer(id) {
         name: "Mosh Hamedani",
         isGold: true,
         email: "email"
-      }).reject(error, console.error("Error", error.message));
+      });
     }, 4000);
   });
 }
@@ -27,14 +33,14 @@ function getTopMovies() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(["movie1", "movie2"]);
-    }, 4000).reject(error => console.error("Error", error.message));
+    }, 4000);
   });
 }
 
 function sendEmail(email, movies) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve().reject(err => console.error("Error", err.message));
+      resolve();
     }, 4000);
   });
 }
